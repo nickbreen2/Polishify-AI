4237c59d650966fef4905e08582a28701ce96678
nickbreen2 <nicolasbre1715@gmail.com>
2026-03-04 16:13:47 -0800

    fixed the favicon issue

---
+++ src/app/icon.tsx
@@ -0,0 +1,26 @@
+import { ImageResponse } from "next/og";
+
+export const runtime = "edge";
+export const size = { width: 32, height: 32 };
+export const contentType = "image/png";
+
+export default function Icon() {
+  return new ImageResponse(
+    (
+      <div
+        style={{
+          background: "#3254F9",
+          width: "100%",
+          height: "100%",
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+          borderRadius: "50%",
+        }}
+      >
+        <span style={{ fontSize: 20, color: "white", lineHeight: 1 }}>✦</span>
+      </div>
+    ),
+    size
+  );
+}
